21b7e1aa8f4e1f003037c8618a444f65c500cceb
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-12-11 00:33:22 +0530

    Added the search bar and end bar with scroll bar being hidden

--- frontend/src/components/FriendListCard..jsx
+++ frontend/src/components/FriendListCard..jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const FriendListCard = ({imgUrl, name}) => {
+    const [deleteButton,setDeleteButton] = useState(true);
     return(
-        <div className="flex w-full h-16 gap-2 items-center mb-2 border-2 border-blue-300 rounded-xl">
+        <div className="relative flex w-full h-16 gap-2 items-center mb-2 border-2 border-purple-300 rounded-md cursor-alias hover:bg-purple-100">
             <div className="w-16 h-full p-2">
                 <img src={imgUrl} alt={name} className="w-full h-full rounded-full object-cover"/>
             </div>
-            <h2 className="font-bold text-[20px]">{name}</h2>
+            <h2 className="font-bold text-[20px] text-gray-600 w-[70%]">{name}</h2>
+            <i onClick={() => setDeleteButton(!deleteButton)} class="ri-arrow-drop-down-line hover:bg-purple-400 rounded-full w-4 h-4 text-center flex items-center cursor-pointer"></i>
+            <div onClick={() => setDeleteButton(true)} className={`absolute -bottom-[15%] right-[5%] w-20 h-8 bg-red-500 rounded-lg ${deleteButton ? 'hidden' : ''} flex justify-center items-center text-white cursor-pointer`}>Remove</div>
         </div>
     )
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 const SearchBar = () => {
     return(
-        <div className="w-full h-16 p-1 flex justify-center items-center gap-2 sticky top-0 bg-blue-100">
-            <input type="text" placeholder="Search" className="w-[85%] h-[80%] p-1 border-2 border-blue-300 rounded-lg outline-0" />
-            <button className="p-1 h-[80%] aspect-square bg-blue-300 hover:bg-blue-400 rounded-lg cursor-pointer"><i className="ri-user-add-line"></i></button>
+        <div className="w-full h-16 p-1 flex justify-center items-center gap-2 sticky top-0 bg-purple-100">
+            <input type="text" placeholder="Search" className="w-[85%] h-[80%] p-1 border-2 border-purple-300 rounded-lg outline-0" />
+            <button className="p-1 h-[80%] aspect-square bg-purple-300 hover:bg-purple-400 rounded-lg cursor-pointer"><i className="ri-user-add-line"></i></button>
         </div>
     )
 }
