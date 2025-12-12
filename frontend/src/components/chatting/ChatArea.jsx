8ac3bebb2ea87818ac3379f65bb9c2afc3e9d6c9
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-12-12 15:55:33 +0530

    some UI changes in chat area

--- frontend/src/components/chatting/ChatArea.jsx
+++ frontend/src/components/chatting/ChatArea.jsx
@@ -12,13 +12,13 @@ const ChatSideBar = () => {
             <div className="absolute right-20 top-25 w-50 h-50 rounded-2xl bg-white/20 rotate-12 shadow-xl "></div>
             <div className="absolute left-20 bottom-25 w-50 h-50 rounded-full bg-white/20 rotate-12 shadow-xl"></div>
 
-            <div className="sticky w-full h-16 px-8 flex items-center gap-5 bg-white/20 backdrop-blur-2xl text-xl">
+            <div className="sticky w-full h-16 px-8 flex items-center gap-5 bg-white/20 backdrop-blur-2xl">
                 <img src="https://randomuser.me/api/portraits/men/32.jpg" 
                 className="h-12 w-12 object-cover overflow-hidden rounded-full"
                 alt="dp" 
                 />
-                <h2>Aisha</h2>
-                <button className="absolute right-8 px-3 py-1 w-fit h-10 rounded-2xl bg-purple-300 cursor-pointer hover:bg-purple-400 hover:shadow-lg border border-white/30 backdrop-blur-lg">Close</button>
+                <h2 className="text-xl">Aisha</h2>
+                <button className="absolute right-8 px-3 py-1 w-fit h-8 flex items-center rounded-2xl bg-purple-300 cursor-pointer hover:bg-purple-400 hover:shadow-lg border border-white/30 backdrop-blur-lg">Close</button>
             </div>
 
             <div className="relative flex-1 min-h-0 flex flex-col">
